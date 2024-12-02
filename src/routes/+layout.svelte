51ece0f3232b70cb6a40c7ab4953be820eca0bc4
Presti <53257574+DxsSucuk@users.noreply.github.com>
2024-12-02 18:31:06 +0100

    Add support widget

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -72,4 +72,13 @@
 			<slot />
 		</div>
 	{/key}
+	<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+	<script>
+	   kofiWidgetOverlay.draw('presti', {
+	   'type': 'floating-chat',
+	   'floating-chat.donateButton.text': 'Support me',
+	   'floating-chat.donateButton.background-color': '#794bc4',
+	   'floating-chat.donateButton.text-color': '#fff'
+	   });
+	</script>
 </AppShell>
